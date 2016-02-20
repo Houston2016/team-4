caca31beb71035d55ca3d87941c801548cf26508
abhmul <abhmul@gmail.com>
2016-02-20 01:52:20 -0600

    Started Presentation

--- Neighborhoods/Results/views.py
+++ Neighborhoods/Results/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def results(request):
     return render(request, 'Results/visualization.html')
 
-def gather_mats(event_id):
-    return MattModel.object.filter(id=event_id)
+def gather_mat():
+    return MattModel.object.filter(submissions)
 
