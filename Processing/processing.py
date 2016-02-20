6a4ab810df54ab7c9d290365b852d512adc8721a
Spencer Chang <spencerc99@gmail.com>
2016-02-19 20:28:23 -0600

    Test case

--- Processing/processing.py
+++ Processing/processing.py
@@ -7,8 +7,10 @@ def sort_data(data):
     """
     mat_to_card_ranking = defaultdict(list)
     for mat in data.keys():
-        for card, ranks in mat.items():
+        for card, ranks in data[mat].items():
             average_rank = float(sum(ranks))/len(ranks)
-            mat_to_card_ranking[mat].append(card, average_rank)
+            mat_to_card_ranking[mat].append((card, average_rank))
     return mat_to_card_ranking
 
+test = {"123123": {"1": [1,2,3,4], "2":[5,5,5,5], "3":[1,1,1,2]}}
+print sort_data(test)
