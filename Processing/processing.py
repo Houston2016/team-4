111cb19b7b764af933ef3b9f1296f81cc422c1fb
Spencer Chang <spencerc99@gmail.com>
2016-02-19 20:58:47 -0600

    Sorting the data in descending order and comments

--- Processing/processing.py
+++ Processing/processing.py
@@ -1,15 +1,25 @@
 from collections import defaultdict
 def sort_data(data):
     """
+    Sorts the data so that it can be visualized
 
-    :param data:
-    :return:
+    Arguments:
+    data -- the data in the form of a dictionary mapping
+    mat id's to a dictionary which maps card names to their
+    respective ranks from different users
+
+    Returns:
+    a dictionary mapping mat ids to a list of tuples containing
+    the card name and average rank in descending order
     """
     mat_to_card_ranking = defaultdict(list)
-    for mat in data.keys():
-        for card, ranks in data[mat].items():
+    for mat_id, mat in data.items():
+        for card, ranks in mat.items():
             average_rank = float(sum(ranks))/len(ranks)
-            mat_to_card_ranking[mat].append((card, average_rank))
+            mat_to_card_ranking[mat_id].append((card, average_rank))
+    # Sorts the mat in descending order
+    for mat_id, mat in mat_to_card_ranking.items():
+        mat.sort(key=lambda tup: tup[1],reverse=True)
     return mat_to_card_ranking
 
 test = {"123123": {"1": [1,2,3,4], "2":[5,5,5,5], "3":[1,1,1,2]}}
