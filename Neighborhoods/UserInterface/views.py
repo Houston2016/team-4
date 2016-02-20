cb596f8591988cd23101846b1e5e0ff6a86701ec
avery jordan <averyjordan1@gmail.com>
2016-02-20 00:09:18 -0600

    added new front end work

--- Neighborhoods/UserInterface/views.py
+++ Neighborhoods/UserInterface/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def mat_view(request):
-    return render(request, 'UserInterface/mats_view.html')
+    return render(request, 'UserInterface/mats_render.html')
 
 def preferences(request):
     return render(request, 'UserInterface/drag_and_drop.html')
