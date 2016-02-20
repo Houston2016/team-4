8b332bd4dc6bd3cd0b811705cc1d200b14357192
alexthkim <atk3@rice.edu>
2016-02-20 08:41:57 -0600

    Working Product

--- Neighborhoods/UserInterface/views.py
+++ Neighborhoods/UserInterface/views.py
@@ -5,11 +5,12 @@ def mat_view(request):
 
 def preferences(request):
     matte_object = MatteModel.objects.filter(title="Maker Space").get()
-    card_list = matte_object.themes.all()
+    card_list = list(matte_object.themes.all())
     min_id_num = 0
     context = {
         'min_id_num':min_id_num,
         'card_list':card_list
     }
+    print card_list
     return render(request, 'UserInterface/glidekim.html', context)
 
