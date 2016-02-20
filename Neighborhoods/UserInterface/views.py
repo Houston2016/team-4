ab0a169389a596165610adddd71364a6bc3df268
Avery Jordan <averyjordan1@gmail.com>
2016-02-20 03:38:52 -0600

    mat revisions

--- Neighborhoods/UserInterface/views.py
+++ Neighborhoods/UserInterface/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def mat_view(request):
     return render(request, 'UserInterface/mats_render.html')
 
-def preferences(request):
-    return render(request, 'UserInterface/drag_and_drop.html')
+def preferences(request,id):
+    return render(id, request, 'UserInterface/drag_and_drop.html')
 
